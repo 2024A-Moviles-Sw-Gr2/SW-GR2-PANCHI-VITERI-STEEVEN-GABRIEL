28d423ff7c7af435e6590936c821b5880ba84d1a
steevenpanchi <steven.panchi13@gmail.com>
2024-05-29 00:41:37 -0500

    Creacion Models, Repositories, Data

---
+++ 02-Deberes/Deber-01/src/main/kotlin/models/Producto.kt
@@ -0,0 +1,49 @@
+package models
+
+import repositories.ProductoRepository
+import java.text.SimpleDateFormat
+import java.util.*
+
+class Producto (
+    val id: Int,
+    val nombre: String,
+    val precio: Double,
+    val fechaCaducidad: Date,
+    val tienda: Tienda
+
+    ){
+
+    private val productoRepository= ProductoRepository(System.getProperty("user.dir")+ "\\src\\main\\kotlin\\data\\Productos.txt")
+
+    fun guardarProducto(){
+        productoRepository.guardarProducto(this)
+        println("Guardando producto: $nombre")
+    }
+
+    fun obtenerProducto(id: Int):Producto{
+        return productoRepository.obtenerProducto(id)
+    }
+
+    fun obtenerProductos():List<Producto>{
+        return productoRepository.obtenerProductos()
+    }
+
+    fun actualizarProducto(){
+        productoRepository.actualizarProducto(this)
+        println("Actualizando producto: $nombre")
+    }
+
+    fun eliminarProducto(){
+        productoRepository.eliminarProducto(this.id)
+        println("Eliminando producto: $nombre")
+    }
+
+    override fun toString(): String {
+        val dateFormat = SimpleDateFormat("dd/MM/yyyy")
+        return "ID Producto: $id " +
+                "Nombre: $nombre " +
+                "Precio: $precio " +
+                "Fecha Caducidad: ${dateFormat.format(fechaCaducidad)} " +
+                "Tienda: $tienda"
+    }
+}
