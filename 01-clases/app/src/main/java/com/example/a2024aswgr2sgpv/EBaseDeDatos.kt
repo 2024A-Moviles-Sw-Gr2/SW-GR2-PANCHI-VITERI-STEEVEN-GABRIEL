fd271cd5f671cbb84d0f723a173fe92ab1b97fe0
steevenpanchi <steven.panchi13@gmail.com>
2024-07-10 23:54:09 -0500

    Last Class

--- 01-clases/app/src/main/java/com/example/a2024aswgr2sgpv/EBaseDeDatos.kt
+++ 01-clases/app/src/main/java/com/example/a2024aswgr2sgpv/EBaseDeDatos.kt
@@ -1,4 +1,7 @@
 package com.example.a2024aswgr2sgpv
 
 class EBaseDeDatos {
+    companion object {
+        var tablaEntrenador: ESqliteHelperEntrenador? = null
+    }
 }
--- 01-clases/app/src/main/java/com/example/a2024aswgr2sgpv/ESqliteHelperEntrenador.kt
+++ 01-clases/app/src/main/java/com/example/a2024aswgr2sgpv/ESqliteHelperEntrenador.kt
@@ -1,4 +1,47 @@
 package com.example.a2024aswgr2sgpv
 
-class ESqliteHelperEntrenador {
-}
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+
+class ESqliteHelperEntrenador(
+        contexto: Context? // this
+    ): SQLiteOpenHelper(
+        contexto,
+        "moviles",
+        null,
+        1
+    ) {
+        override fun onCreate(db: SQLiteDatabase?) {
+            val scriptSQLCrearTablaEntrenador =
+                """
+                CREATE TABLE ENTRENADOR(
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    nombre VARCHAR(50),
+                    descripcion VARCHAR(50)
+                )
+            """.trimIndent()
+            db?.execSQL(scriptSQLCrearTablaEntrenador)
+        }
+        override fun onUpgrade(
+            p0: SQLiteDatabase?, p1: Int, p2: Int) {}
+        fun crearEntrenador(
+            nombre: String,
+            descripcion:String
+        ):Boolean {
+            val basedatosEscritura = writableDatabase
+            val valoresAGuardar = ContentValues()
+            valoresAGuardar.put("nombre", nombre)
+            valoresAGuardar.put("descripcion", descripcion)
+            val resultadoGuardar = basedatosEscritura
+                .insert(
+                    "ENTRENADOR", // nombre tabla
+                    null,
+                    valoresAGuardar // valores
+                )
+            basedatosEscritura.close()
+            return if(resultadoGuardar.toInt() == -1) false else true
+        }
+    }
